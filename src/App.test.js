8290f0248d310ec3298547c6b7b43fa3d0e9765a
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-10-05 13:29:05 +0530

    Update App.test.js

---
+++ src/App.test.js
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import App from "./App";
+
+test("can receive a new user and show it on a list", () => {
+  render(<App />);
+
+  const nameInput = screen.getByRole("textbox", {
+    name: /name/i,
+  });
+  const emailInput = screen.getByRole("textbox", {
+    name: /email/i,
+  });
+  const button = screen.getByRole("button");
+
+  user.click(nameInput);
+  user.keyboard("anto");
+  user.click(emailInput);
+  user.keyboard("anto@gmail.com");
+
+  user.click(button);
+
+  const name = screen.getByRole("cell", { name: "anto" });
+  const email = screen.getByRole("cell", { name: "anto@gmail.com" });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
+});
