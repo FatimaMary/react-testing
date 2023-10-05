2e85da74870cd10f818726dd5890bbde11201290
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-10-05 12:37:41 +0530

    User List test updated

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,17 +1,37 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import UserList from "./UserList";
 
-test("render one roe per user", () => {
-  //Render the component
+function renderComponent() {
   const users = [
     { name: "anto", email: "anto@gmail.com" },
     { name: "fati", email: "fati@gmail.com" },
   ];
   render(<UserList users={users} />);
 
+  return {
+    users,
+  };
+}
+
+test("render one roe per user", () => {
+  //Render the component
+  renderComponent();
   //Find all the rows in the table
+  //eslint-disable-next-line
+  const rows = within(screen.getByTestId("users")).getAllByRole("row");
 
   //Assertation: correct number of rows in the table
+  expect(rows).toHaveLength(2);
 });
 
-test("render the email and name of each user", () => {});
+test("render the email and name of each user", () => {
+  const { users } = renderComponent();
+
+  for (let user of users) {
+    const name = screen.getByRole("cell", { name: user.name });
+    const email = screen.getByRole("cell", { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
+});
